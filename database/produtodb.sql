faa728ceb4d3a1c3a387d7473bc6c0f6e4d20bf4
THIAGO DE JESUS BEZERRA <thiago.jbezerra@senacsp.edu.br>
2019-05-17 16:16:25 -0300

    Criação do Banco de Dados

---
+++ database/produtodb.sql
@@ -0,0 +1,14 @@
+create database produtodb;
+use produtodb;
+create table tbproduto(
+id int auto_increment primary key,
+nomeproduto varchar(50) not null,
+descricaoproduto varchar(100) not null,
+fabricante varchar(50) not null,
+quantidade int not null,
+preco decimal(10,2) not null
+);
+insert into tbproduto(nomeproduto,descricaoproduto,fabricante,quantidade,preco)
+values('trakinas','bolacha com recheio','mondelez',100,2.49);
+
+select * from tbproduto;
